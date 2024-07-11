7d51e3378e19c8298e3a13279d89a5529f0dc7d7
dydy5555 <biijudy07@gmail.com>
2024-07-11 09:46:14 +0700

    rady

--- src/app/app/employee-assets/page.tsx
+++ src/app/app/employee-assets/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import PageNavbar, { PageNavbarLeftContent, PageNavbarRightContent } from '@/components/layout/PageNavbar'
-import { Profile } from 'iconsax-react'
+import { Buildings, Profile } from 'iconsax-react'
 import ListUsers from '@/components/ListUsers'
 import PageContent from '@/components/Layout/PageContent'
 function Page() {
@@ -10,8 +10,9 @@ function Page() {
 
 
     return (
-        <div className='text-gray-500 w-full p-10'>
+        <div className='text-gray-500 w-full px-10 py-10'>
                 {/* users table */}
+                <p className='text-lg font-medium  text-black border-b-[0.5px] pb-5 mb-5'> <Buildings size="32" color="#FF8A65"/> Assets Employee</p>
                 <ListUsers />
         </div>
     )
